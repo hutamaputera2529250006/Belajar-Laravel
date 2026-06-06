de7d9268356dfb47ce2c8ab7ab861ae2782a9a2e
Hutama Putera <hutamaputera_2529250006@mhs.mdp.ac.id>
2026-06-06 15:09:13 +0700

    Blended3Juni2026

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -12,7 +12,9 @@ use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Actions\RedirectIfTwoFactorAuthenticatable;
+use Laravel\Fortify\Contracts\RegisterResponse as RegisterResponseContract;
 use Laravel\Fortify\Fortify;
+use Laravel\Fortify\Http\Responses\LoginResponse as LoginResponseContract;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -21,8 +23,42 @@ class FortifyServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(
+            RegisterResponseContract::class,
+            function (){
+                return new class implements RegisterResponseContract{
+                    public function toResponse($request){
+                        if($request->expectsJson()){
+                            return response()->json([
+                                'success'=> true,
+                            ]);
+                        }
+                        return redirect()->intended(Fortify::redirects('register'));
+                    }
+                };
+            }
+        );
+
+        $this->app->singleton(
+            LoginResponseContract::class,
+            function(){
+                return new class implements LoginResponseContract{
+                    public function toResponse($request){
+                        if($request->expectsJson()){
+                            return response()->json([
+                                'success'=> true,
+                                'user' => $request->user(),
+                                'token'=>$request->user->createToken('api')->plainTextToken
+                            ]);
+                        }
+                        return redirect()->intended(Fortify::redirects('login'));
+                    }
+                };
+            }
+        );
     }
+
+
 
     /**
      * Bootstrap any application services.
